182accd5e167c32c9eecd355c9fe74659be4fd2b
tonyhabib01 <34315675+tonyhabib01@users.noreply.github.com>
2021-04-13 15:44:02 +0300

    fix about us

--- src/app/previous-work-image/shared/previous-work-image.service.ts
+++ src/app/previous-work-image/shared/previous-work-image.service.ts
@@ -14,13 +14,13 @@ export class PreviousWorkImageService{
 }
 
 const IMAGES = [
-  {id: 1, url: '../../assets/img/scenery/image1.jpg'},
-  {id: 2, url: '../../assets/img/scenery/image4.jpg'},
-  {id: 3, url: '../../assets/img/scenery/image6.jpg'},
-  {id: 4, url: '../../assets/img/scenery/image5.jpg'},
-  {id: 5, url: '../../assets/img/scenery/image1.jpg'},
-  {id: 6, url: '../../assets/img/scenery/image4.jpg'},
-  {id: 7, url: '../../assets/img/scenery/image6.jpg'},
-  {id: 8, url: '../../assets/img/scenery/image5.jpg'},
-  {id: 9, url: '../../assets/img/scenery/image1.jpg'},
+  {id: 1, url: './assets/img/scenery/image1.jpg'},
+  {id: 2, url: './assets/img/scenery/image4.jpg'},
+  {id: 3, url: './assets/img/scenery/image6.jpg'},
+  {id: 4, url: './assets/img/scenery/image5.jpg'},
+  {id: 5, url: './assets/img/scenery/image1.jpg'},
+  {id: 6, url: './assets/img/scenery/image4.jpg'},
+  {id: 7, url: './assets/img/scenery/image6.jpg'},
+  {id: 8, url: './assets/img/scenery/image5.jpg'},
+  {id: 9, url: './assets/img/scenery/image1.jpg'},
 ];
